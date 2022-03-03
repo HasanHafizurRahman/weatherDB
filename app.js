44701ccd9892c167d4daa016110bfb5ebe1d7ae7
HasanHafizurRahman <hasanshanto922@gmail.com>
2022-03-04 00:16:29 +0600

    city description added

--- app.js
+++ app.js
@@ -2,13 +2,20 @@
 
 const searchTemperature = () => {
      const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     fetch(url)
     .then(res => res.json())
     .then(data => displayTemperature(data))
     
 } 
 
+const setInnerText = (id, text) =>{
+    document.getElementById(id).innerText = text;
+}
 const displayTemperature = temperature => {
-    console.log(temperature)
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('cloud', temperature.weather[0].description)
+    console.log(temperature);
+
 }
